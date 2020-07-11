1090f30bc32472f4363bb0502a7f6df93d3738f6
anujnaRao <anujna.p.rao@gmail.com>
2020-07-11 22:11:34 +0530

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponents';
+import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
-import Footer from './FooterComponent';
+import Footer from './FooterComponent'; 
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -31,6 +32,13 @@ class Main extends Component {
                     leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
         );
       }
+      
+    const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        );
+      };
 
     return (
         <div>
@@ -38,6 +46,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />} />
                     <Redirect to="/home" />
                 </Switch>
