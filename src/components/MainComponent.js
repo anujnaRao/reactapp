938434873103693d7a48b5b05d991c1d3225231b
anujnaRao <anujna.p.rao@gmail.com>
2020-07-12 19:40:08 +0530

    Route and SPA

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Menu from './MenuComponents';
 import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent'; 
 import { DISHES } from '../shared/dishes';
@@ -39,15 +40,16 @@ class Main extends Component {
               comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
         );
       };
-
+ 
     return (
         <div>
             <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
+                    <Route path='/aboutus'  component={() => <About leaders ={this.state.leaders}/>} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
-                    <Route exact path='/contactus' component={Contact} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
             <Footer />
